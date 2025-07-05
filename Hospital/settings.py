69b0199d7c193617c72839f344237064e7619e5e
Hasan Balleh <hassanballeh@gmail.com>
2025-07-05 23:53:26 +0300

    add settings

--- Hospital/settings.py
+++ Hospital/settings.py
@@ -37,7 +37,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "django_filters",
+    "Patient",
 ]
+
+REST_FRAMEWORK = { 
+"DEFAULT_FILTER_BACKENDS": 
+["django_filters.rest_framework.DjangoFilterBackend"] 
+} 
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
