568fb31616acd39f9b9e7bf16821642e8d8c4d86
Sonar Masri <sonarmasri55@gmail.com>
2025-07-06 01:13:46 +0100

    add patient model

--- Patient/models.py
+++ Patient/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    firstName=models.CharField(max_length=20)
+    lastName=models.CharField(max_length=20)
+    birthDate=models.DateField()
+    phoneNumber=models.CharField(max_length=14)
+    isInsured= models.BooleanField(default=False)
