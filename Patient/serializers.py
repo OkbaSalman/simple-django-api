fb732e473fde6917987e7cb339b490f786c94053
Abd Alrahman Haddad <abodehaddad15@gmail.com>
2025-07-06 12:21:41 +0300

    add views

--- Patient/serializers.py
+++ Patient/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers 
 from .models import Patient
-class TaskSerializer(serializers.ModelSerializer):  
+class PatientSerializer(serializers.ModelSerializer):  
     class Meta:
         model = Patient
         fields = 'all'
--- Patient/views.py
+++ Patient/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Patient
+from .serializers import PatientSerializer
 # Create your views here.
+class PatientViewSet(viewsets.ModelViewSet):
+    queryset = Patient.objects.all()
+    serializer_class=PatientSerializer
+    filter_backends=[DjangoFilterBackend]
+    filterset_fields=["isInsured"]
