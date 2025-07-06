fbdc05b3ab19196f46f2dfcef00abe738850900d
unknown <khaldondhhan555@gmail.com>
2025-07-06 09:39:24 -0400

    update urls

--- Hospital/urls.py
+++ Hospital/urls.py
@@ -1,7 +1,7 @@
 """
-URL configuration for Hospital project.
+URL configuration for ToDo project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The urlpatterns list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from Patient.views import PatientViewSet
+router = DefaultRouter()
+router.register(r"patients", PatientViewSet)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/",admin.site.urls),
+    path("api/", include(router.urls)),  
 ]
